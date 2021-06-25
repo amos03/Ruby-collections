467686e06dd1cfec68dcf2b22a9622e17187d507
amos03 <amossi@gmail.com>
2021-06-24 23:10:54 -0400

    Exercise 0 set up arrays

---
+++ exercises0-10.rb
@@ -0,0 +1,11 @@
+colors = ["blue", "grey", "black", "light blue", "silver"]
+ages_of_siblings_cousins = [40, 39, 24, 38, 34, 28, 22]
+coin_toss_heads =[true, true, false, true, false]
+performing_artists=["Ishay Ribo", "Chanan ben Ari," "Idan Raichel"]
+colors_symbols=[:"blue", :"grey", :"black", :"light_blue", :"silver"] 
+
+p colors
+p ages_of_siblings_cousins
+p coin_toss_heads
+p performing_artists
+p colors_symbols
