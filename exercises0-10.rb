d5f9d698dec67809487ea139ec2d35a12cf2ce3b
amos03 <amossi@gmail.com>
2021-06-25 10:27:55 -0400

    setting up hashes

--- exercises0-10.rb
+++ exercises0-10.rb
@@ -1,3 +1,4 @@
+#Exercise 0.1: Setting up arrays
 colors = ["blue", "grey", "black", "light blue", "silver"]
 ages_of_siblings_cousins = [40, 39, 24, 38, 34, 28, 22]
 coin_toss_heads =[true, true, false, true, false]
@@ -9,3 +10,14 @@ p ages_of_siblings_cousins
 p coin_toss_heads
 p performing_artists
 p colors_symbols
+
+#Exercise 0.2: Setting up hashes
+dictionary = {logomachy: "An argument about words.", cryptozoology: "The study of animals whose existence has not yet been proven.", doryphore: "A vexatious critic of others, often focused on minutiae."}
+films = [{name: "The Departed", year: 2006}, {name: "Footnote", year: 2011}, {name: "Seven Samurai", year: 1954}]
+populations_cities = {Seoul: 97600000, Palo_Alto: 56000, Tekoa: 4076}
+friends_ages = {Noah: 40, Chaim: 33, Robert: 38, Jun: 50}
+
+p dictionary
+p films
+p populations_cities
+p friends_ages
