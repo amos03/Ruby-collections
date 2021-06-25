f77620bade66a1cf3260710229a8c03f7a65058b
amos03 <amossi@gmail.com>
2021-06-25 10:52:35 -0400

    first and last items of arrays printed using methods

--- exercises0-10.rb
+++ exercises0-10.rb
@@ -2,7 +2,7 @@
 colors = ["blue", "grey", "black", "light blue", "silver"]
 ages_of_siblings_cousins = [40, 39, 24, 38, 34, 28, 22]
 coin_toss_heads =[true, true, false, true, false]
-performing_artists=["Ishay Ribo", "Chanan ben Ari," "Idan Raichel"]
+performing_artists=["Ishay Ribo", "Chanan ben Ari", "Idan Raichel"]
 colors_symbols=[:"blue", :"grey", :"black", :"light_blue", :"silver"] 
 
 #Exercise 0.2: Setting up hashes
@@ -13,24 +13,39 @@ friends_ages = {Noah: 40, Chaim: 33, Robert: 38, Jun: 50}
 
 #Exercise 1
 
-names_of_my_arrays = ["colors", "ages_of_siblings_cousins", "coin_toss_heads", "performing_artists", "colors_symbols"]
+names_of_my_arrays = [colors, ages_of_siblings_cousins, coin_toss_heads, performing_artists, colors_symbols]
+
 
 def print_array_first_item (array_name)
     p array_name[0]
 end
 
+# def print_all_my_arrays_first_items (names_array)
+#         names_array.each do |name_of_array|
+#         print_array_first_item (names_array[name_of_array])
+#     end
+# end
+
+# print_all_my_arrays_first_items(names_of_my_arrays)
+
+
 def print_array_last_item (array_name)
     p array_name[-1]
 end
 
+puts "---------------------------"
+puts "First Item of Each Array:"
 print_array_first_item(colors)
 print_array_first_item(ages_of_siblings_cousins)
 print_array_first_item(coin_toss_heads)
 print_array_first_item(performing_artists)
 print_array_first_item(colors_symbols)
+puts "---------------------------"
 
-print_array_first_item(colors)
-print_array_first_item(ages_of_siblings_cousins)
-print_array_first_item(coin_toss_heads)
-print_array_first_item(performing_artists)
-print_array_first_item(colors_symbols)
+puts "Last Item of Each Array:"
+print_array_last_item(colors)
+print_array_last_item(ages_of_siblings_cousins)
+print_array_last_item(coin_toss_heads)
+print_array_last_item(performing_artists)
+print_array_last_item(colors_symbols)
+puts "---------------------------"
