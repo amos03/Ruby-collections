98259e2e5323714506609812c93eabba020a1b76
amos03 <amossi@gmail.com>
2021-06-25 10:37:59 -0400

    Method for printing first item

--- exercises0-10.rb
+++ exercises0-10.rb
@@ -5,19 +5,32 @@ coin_toss_heads =[true, true, false, true, false]
 performing_artists=["Ishay Ribo", "Chanan ben Ari," "Idan Raichel"]
 colors_symbols=[:"blue", :"grey", :"black", :"light_blue", :"silver"] 
 
-p colors
-p ages_of_siblings_cousins
-p coin_toss_heads
-p performing_artists
-p colors_symbols
-
 #Exercise 0.2: Setting up hashes
 dictionary = {logomachy: "An argument about words.", cryptozoology: "The study of animals whose existence has not yet been proven.", doryphore: "A vexatious critic of others, often focused on minutiae."}
 films = [{name: "The Departed", year: 2006}, {name: "Footnote", year: 2011}, {name: "Seven Samurai", year: 1954}]
 populations_cities = {Seoul: 97600000, Palo_Alto: 56000, Tekoa: 4076}
 friends_ages = {Noah: 40, Chaim: 33, Robert: 38, Jun: 50}
 
-p dictionary
-p films
-p populations_cities
-p friends_ages
+#Exercise 1
+
+names_of_my_arrays = ["colors", "ages_of_siblings_cousins", "coin_toss_heads", "performing_artists", "colors_symbols"]
+
+def print_array_first_item (array_name)
+    p array_name[0]
+end
+
+def print_array_last_item (array_name)
+    p array_name[-1]
+end
+
+print_array_first_item(colors)
+print_array_first_item(ages_of_siblings_cousins)
+print_array_first_item(coin_toss_heads)
+print_array_first_item(performing_artists)
+print_array_first_item(colors_symbols)
+
+print_array_first_item(colors)
+print_array_first_item(ages_of_siblings_cousins)
+print_array_first_item(coin_toss_heads)
+print_array_first_item(performing_artists)
+print_array_first_item(colors_symbols)
